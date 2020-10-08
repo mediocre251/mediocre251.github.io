f73479fc801dd9b1db38744795d9f7a7fe71ff5f
mediocre251 <70655924+mediocre251@users.noreply.github.com>
2020-10-08 12:40:54 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -94,18 +94,468 @@ function subt1 (elem) {
   }
 }
 
-elem[3] = document.getElementById("3");
+elem[4] = document.getElementById("4");
 // display the number of children (all td elements)
 // console.log(elem.children.length);
 // display the content of the + button, which is the first child of the fifth element
 // console.log(elem.children[4].children[0]); 
 
 // assign a function to the + button
-elem[3].children[4].children[0].onclick 
-  = function(){add1(elem[3]);};
+elem[4].children[4].children[0].onclick 
+  = function(){add1(elem[4]);};
 
-elem[3].children[4].children[1].onclick
-  = function(){subt1(elem[3]);};
+elem[4].children[4].children[1].onclick
+  = function(){subt1(elem[4]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+
+
+
+elem[5] = document.getElementById("4");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+
+// assign a function to the + button
+elem[5].children[4].children[0].onclick 
+  = function(){add1(elem[5]);};
+
+elem[5].children[4].children[1].onclick
+  = function(){subt1(elem[5]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+}
+}
+
+
+elem[6] = document.getElementById("6");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[6].children[4].children[0].onclick 
+  = function(){add1(elem[6]);};
+
+elem[6].children[4].children[1].onclick
+  = function(){subt1(elem[6]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+elem[7] = document.getElementById("7");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[7].children[4].children[0].onclick 
+  = function(){add1(elem[7]);};
+
+elem[7].children[4].children[1].onclick
+  = function(){subt1(elem[7]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+elem[8] = document.getElementById("8");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[8].children[4].children[0].onclick 
+  = function(){add1(elem[8]);};
+
+elem[8].children[4].children[1].onclick
+  = function(){subt1(elem[8]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+
+
+
+
+elem[9] = document.getElementById("9");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[9].children[4].children[0].onclick 
+  = function(){add1(elem[9]);};
+
+elem[9].children[4].children[1].onclick
+  = function(){subt1(elem[9]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+}
+}
+
+
+elem[10] = document.getElementById("10");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[10].children[4].children[0].onclick 
+  = function(){add1(elem[10]);};
+
+elem[10].children[4].children[1].onclick
+  = function(){subt1(elem[10]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+elem[11] = document.getElementById("11");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[11].children[4].children[0].onclick 
+  = function(){add1(elem[11]);};
+
+elem[11].children[4].children[1].onclick
+  = function(){subt1(elem[11]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+elem[12] = document.getElementById("12");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[12].children[4].children[0].onclick 
+  = function(){add1(elem[12]);};
+
+elem[12].children[4].children[1].onclick
+  = function(){subt1(elem[12]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+
+
+elem[13] = document.getElementById("13");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[13].children[4].children[0].onclick 
+  = function(){add1(elem[13]);};
+
+elem[13].children[4].children[1].onclick
+  = function(){subt1(elem[13]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+}
+}
+
+
+elem[14] = document.getElementById("14");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[14].children[4].children[0].onclick 
+  = function(){add1(elem[14]);};
+
+elem[14].children[4].children[1].onclick
+  = function(){subt1(elem[14]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+elem[15] = document.getElementById("15");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[15].children[4].children[0].onclick 
+  = function(){add1(elem[15]);};
+
+elem[15].children[4].children[1].onclick
+  = function(){subt1(elem[15]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+elem[16] = document.getElementById("16");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[16].children[4].children[0].onclick 
+  = function(){add1(elem[16]);};
+
+elem[16].children[4].children[1].onclick
+  = function(){subt1(elem[16]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
+
+
+elem[17] = document.getElementById("17");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[17].children[4].children[0].onclick 
+  = function(){add1(elem[17]);};
+
+elem[17].children[4].children[1].onclick
+  = function(){subt1(elem[17]);};
+
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") 
+    elem.children[2].innerHTML = "1";
+  else if(elem.children[2].innerHTML <"8") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+  }
+}
+function subt1 (elem) {
+  if(elem.children[2].innerHTML > "0") {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+}
+}
+
+
+elem[18] = document.getElementById("18");
+// display the number of children (all td elements)
+// console.log(elem.children.length);
+// display the content of the + button, which is the first child of the fifth element
+// console.log(elem.children[4].children[0]); 
+
+// assign a function to the + button
+elem[18].children[4].children[0].onclick 
+  = function(){add1(elem[18]);};
+
+elem[18].children[4].children[1].onclick
+  = function(){subt1(elem[18]);};
 
 // create an "add1" function
 function add1 (elem) {
