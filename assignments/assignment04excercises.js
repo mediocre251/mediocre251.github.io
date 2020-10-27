8ed6a368628d68bd1a11947984e43f03b592484a
mediocre251 <70655924+mediocre251@users.noreply.github.com>
2020-10-27 14:31:16 -0400

    Update assignment04excercises.js

--- assignments/assignment04excercises.js
+++ assignments/assignment04excercises.js
@@ -117,24 +117,35 @@ function appendTableRow5 (tableobj, col1, col2, col3,col4,col5) {
 // 8. create "totals" row and column in a table
 
 let div3B = document.getElementById("3B");
+let table3A= document.getElementById("3A");
 let table3B = createTable("table3B");
 div3B.appendChild(table3B);
-let p00 = 2;//try .innerText
-let p01 = 2;
-table3B.setAttribute("style", "border:1px solid black;")
-table3B.setAttribute("width", "100%")
-addrows(table3B,"Thingamabob","1.00","1","space");
-addrows(table3B,"Whachamacallit","2.00","2","space");
-addrows(table3B,"Doohickey","3.00","3","space");
-addrows(table3B,"Totals:",p00*p01,"space","space");
+
+
+
+let p00 = parseInt(table3A.children[0].children[0].children[1].children[1].innerHTML);
+let p01 = parseInt(table3A.children[0].children[0].children[2].children[1].innerHTML);
+let p02 = parseInt(table3A.children[0].children[0].children[3].children[1].innerHTML);
+let p10 = parseInt(table3A.children[0].children[0].children[1].children[2].innerHTML);
+let p11 = parseInt(table3A.children[0].children[0].children[2].children[2].innerHTML);
+let p12 = parseInt(table3A.children[0].children[0].children[3].children[2].innerHTML);
+table3B.setAttribute("style", "border:1px solid black;");
+table3B.setAttribute("width", "100%");
+addrows(table3B,"item","price","qty","Price * Qty");
+addrows(table3B,"Thingamabob","1.00","1",p00*p10);
+addrows(table3B,"Whachamacallit","2.00","2",p01*p11);
+addrows(table3B,"Doohickey","3.00","3",p02*p12);
+addrows(table3B,"Totals:",p00+p01+p02,p10+p11+p12,p02*p12+p01*p11+p00*p10);
 
 // append to tableobj a 3-column table row 
-function addrows (tableobj, col1, col2, col3, col4) {
+function addrows (tableobj,coltop, col1, col2, col3, col4) {
+  let td0 = document.createElement("td");
   let td1 = document.createElement("td");
   let td2 = document.createElement("td");
   let td3 = document.createElement("td");
   let td4 = document.createElement("td");
   // insert content into columns
+  td0.innerHTML = coltop;
   td1.innerHTML = col1;
   td2.innerHTML = col2;
   td3.innerHTML = col3;
@@ -143,10 +154,10 @@ function addrows (tableobj, col1, col2, col3, col4) {
   // create table row DOM object
   let tr = document.createElement("tr");
   // append table divisions (columns) to table row
+  tr.appendChild(td0);
   tr.appendChild(td1);
   tr.appendChild(td2);
   tr.appendChild(td3);
-  tr.appendChild(td4);
   // append the row to the tbody element in the table
   tableobj.children[0].appendChild(tr);
 }
